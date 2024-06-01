a54f4f0bd81bf29cb9529d7be9722ba98103969e
yardenon7 <yardenon7@gmail.com>
2024-06-01 19:11:00 +0300

    l

--- Client.py
+++ Client.py
@@ -12,44 +12,58 @@ PORT = 1729
 WINDOW_WIDTH = 1373
 WINDOW_HEIGHT = 810
 WHITE = (255, 255, 255)
-COLOR_KEY = 154, 102, 10
+COLOR_KEY = 154, 102, 19
+GREEN_COLOR = (0, 255, 0)
 BACKGROUND = 'CatBackground.jpg'
 CAT = ["Cat0.png", "Cat1.png", "Cat2.png", "Cat3.png", "Cat4.png", "Cat5.png", "Cat6.png", "Cat7.png", "Cat8.png", "Cat9.png", "CatPeek.png", "CatDraw2.png", "CatBlank.png"]
 CARD_WIDTH = 181
 CARD_HEIGHT = 240
 CAT12= 'CatBackCard.png'
+PLACEMENT_USED_CARDS= (1000, 200)
+PLACEMENT_NEW_CARD = (600, 200)
+PLACEMENT_BACK_CARD = (400, 200)
+PLACEMENT_START_OF_SCREEN = (0, 0)
+ZERO = 0
+ONE = 1
+THREE = 3
+ELEVEN = 11
 used_cards = [12]
 back_card_rect=pygame.Rect(400, 200, CARD_WIDTH, CARD_HEIGHT)
 cat_used_cards_rect = pygame.Rect(1000, 200, CARD_WIDTH, CARD_HEIGHT)
 card_rects = []
 #card_rects = [(280, 540, 181, 240), (480, 540, 181, 240), (680, 540, 181, 240), (880, 540, 181, 240)]
 #card_rects = [<rect(280, 540, 181, 240)>, <rect(480, 540, 181, 240)>, <rect(680, 540, 181, 240)>, <rect(880, 540, 181, 240)>, <rect(280, 540, 181, 240)>, <rect(480, 540, 181, 240)>, <rect(680, 540, 181, 240)>, <rect(880, 540, 181, 240)>]
-current_card = 0
+current_card = ZERO
 show_new_card = False
 #the amount of every card in the stack
+
+numbers = []
+set_of_cards = []
+'''
 numbers = [i for i in range(7)] * 4 + [7, 8] * 5 + [9] * 7 + [10, 10, 10, 11, 11, 11]
 selected_numbers = random.sample(numbers, 4)
 set_of_cards = SetOfCards(selected_numbers)
 for number in selected_numbers:
     numbers.remove(number)
+'''
 pygame.init()
- #fgh
+
 def create_new_screen(screen):
     global card_rects
     pygame.display.set_caption("RatATat")
     background = pygame.image.load(BACKGROUND)
-    screen.blit(background, (0, 0))
+    screen.blit(background, PLACEMENT_START_OF_SCREEN)
     #pygame.display.flip()
     cat_back_card = pygame.image.load(CAT12)
-    cat_back_card.set_colorkey((154, 102, 19))
+    cat_back_card.set_colorkey(COLOR_KEY)
     cat_back_card = pygame.transform.scale(cat_back_card, (CARD_WIDTH, CARD_HEIGHT))
     #global back_card_rect
     #back_card_rect= cat_back_card.get_rect(topleft=(400, 200))
-    screen.blit(cat_back_card, (400, 200))
+    screen.blit(cat_back_card, PLACEMENT_BACK_CARD)
     cat_used_cards = pygame.image.load(CAT[used_cards[-1]])
-    cat_used_cards.set_colorkey((154, 102, 19))
+    cat_used_cards.set_colorkey(COLOR_KEY)
     cat_used_cards = pygame.transform.scale(cat_used_cards, (CARD_WIDTH, CARD_HEIGHT))
-    screen.blit(cat_used_cards, (1000, 200))
+    screen.blit(cat_used_cards, PLACEMENT_USED_CARDS)
 
     card_rects = []
     #if show_new_card:
@@ -63,7 +77,7 @@ def create_new_screen(screen):
     for i in range(4):
 
         card_image = pygame.image.load(CAT[set_of_cards.get_cards()[i]])
-        card_image.set_colorkey((154, 102, 19))
+        card_image.set_colorkey(COLOR_KEY)
         card_image = pygame.transform.scale(card_image, (CARD_WIDTH, CARD_HEIGHT))
         #screen.blit(card_image, (280+200*i, 540))
         card_rect = card_image.get_rect(topleft=(280 + 200 * i, 540))
@@ -72,15 +86,16 @@ def create_new_screen(screen):
     pygame.display.flip()
     return screen
 
+
 def draw_the_card(screen):
     global show_new_card
     global current_card
     current_card = random.choice(numbers)
     numbers.remove(current_card)
     new_card = pygame.image.load(CAT[current_card])
-    new_card.set_colorkey((154, 102, 19))
+    new_card.set_colorkey(COLOR_KEY)
     new_card = pygame.transform.scale(new_card, (CARD_WIDTH, CARD_HEIGHT))
-    screen.blit(new_card, (600, 200))
+    screen.blit(new_card, PLACEMENT_NEW_CARD)
     pygame.display.flip()
     return current_card
 
@@ -94,15 +109,15 @@ def handle_mouse_click(event, screen):
         show_new_card = True
         create_new_screen(screen)
         current_card = draw_the_card(screen)
-        if current_card== 11:
+        if current_card== ELEVEN:
             used_cards.append(current_card)
             time.sleep(0.5)
             create_new_screen(screen)
             current_card = draw_the_card(screen)
             return True
-    if cat_used_cards_rect.collidepoint(mouse_x, mouse_y) and not show_new_card and not len(used_cards) == 1:
+    if cat_used_cards_rect.collidepoint(mouse_x, mouse_y) and not show_new_card and not len(used_cards) == ONE:
         print(3)
-        pygame.draw.rect(screen, (0, 255, 0), cat_used_cards_rect, 3)
+        pygame.draw.rect(screen, GREEN_COLOR, cat_used_cards_rect, THREE)
         pygame.display.flip()
     if show_new_card:
         t=False
@@ -118,7 +133,7 @@ def handle_mouse_click(event, screen):
             t = True
         if t:
             create_new_screen(screen)
-    if screen.get_at((1000, 200)) == (0,255,0):
+    if screen.get_at(PLACEMENT_USED_CARDS) == GREEN_COLOR:
         print(5)
         for i, card_rect in enumerate(card_rects):
             if card_rect.collidepoint(mouse_x, mouse_y):
@@ -129,6 +144,7 @@ def handle_mouse_click(event, screen):
 
     return False
 
+
 def draw_two_case(screen, event, count):
     global current_card
     mouse_x, mouse_y = event.pos
@@ -137,18 +153,18 @@ def draw_two_case(screen, event, count):
             used_cards.append(set_of_cards.get_a_specific_card(i))
             set_of_cards.set_a_card(i, current_card)
             create_new_screen(screen)
-            if count == 0:
+            if count == ZERO:
                 print(1)
                 current_card = draw_the_card(screen)
-            return 1
+            return ONE
     if cat_used_cards_rect.collidepoint(mouse_x, mouse_y):
         used_cards.append(current_card)
         create_new_screen(screen)
-        if count == 0:
+        if count == ZERO:
             print(1)
             current_card = draw_the_card(screen)
-        return 1
-    return 0
+        return ONE
+    return ZERO
 
 
 
@@ -160,6 +176,9 @@ def draw_two_case(screen, event, count):
 
 
 def main():
+    global numbers
+    global used_cards
+    global set_of_cards
     all_info = [numbers, used_cards, set_of_cards]
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -168,8 +187,7 @@ def main():
     except socket.error as err:
         print('received socket error ' + str(err))
 
-
-    count_for_draw_two=0
+    count_for_draw_two=ZERO
     is_it_draw_two = False
     size = (WINDOW_WIDTH, WINDOW_HEIGHT)
     screen = pygame.display.set_mode(size)
@@ -188,20 +206,20 @@ def main():
         while not my_turn:
             rlist, _, _ = select.select([client_socket], [], [])
             print(rlist)
-            if client_socket in rlist:
+            if rlist:
                 print("not empty")
-                sock = rlist[0]
+                sock = rlist[ZERO]
                 response = protocol_decryption_request(sock)
                 print(response)
             screen = create_new_screen(screen)
-            if response.startswith('start'):
+            if response.startswith("It's"):
                 my_turn = True
             elif response == 'ratatat':
                 print(3)
             else:
-                numbers = response[0]
-                used_cards = response[1]
-
+                numbers = response[ZERO]
+                used_cards = response[ONE]
+                set_of_cards = response[2]
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 finish = True
@@ -210,12 +228,9 @@ def main():
                     count_for_draw_two += draw_two_case(screen, event, count_for_draw_two)
                 else:
                     is_it_draw_two = handle_mouse_click(event, screen)
-                if count_for_draw_two==1:
-                    count_for_draw_two=0
+                if count_for_draw_two==ONE:
+                    count_for_draw_two=ZERO
                     is_it_draw_two = False
-
-
-
 
 
 pygame.quit()
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import socket
 from Protocoly import *
+import random
+from SetOfCards import SetOfCards
+
 IP = '0.0.0.0'
 PORT = 1729
 QUEUE_LEN = 4
-four_decks=[[], [], [], []]
+four_decks=[]
+numbers = [i for i in range(7)] * 4 + [7, 8] * 5 + [9] * 7 + [10, 10, 10, 11, 11, 11]
+used_cards = [12]
 
 def get_valid_integer():
     while True:
@@ -12,7 +17,19 @@ def get_valid_integer():
             number = int(user_input)
             if 2 <= number <= 4:
                 return number
+
+
+def make_the_decks():
+    for deck in four_decks:
+        selected_numbers = random.sample(numbers, 4)
+        four_decks[deck] = SetOfCards(selected_numbers)
+        for number in selected_numbers:
+            numbers.remove(number)
+
+
 def main():
+    global numbers
+    global used_cards
     current_deck=-1
     number_of_players= get_valid_integer()
     client_sockets = []
@@ -20,7 +37,7 @@ def main():
     try:
         server_socket.bind((IP, PORT))
         server_socket.listen(QUEUE_LEN)
-        while len(client_sockets) < number_of_players:
+        while len(client_sockets) < number_of_players-1:
             client_socket, address = server_socket.accept()
             print(f"Client connected from {address}")
             client_sockets.append(client_socket)
@@ -31,7 +48,7 @@ def main():
                     current_deck=0
                 client_socket, client_address = server_socket.accept()
                 try:
-                    protocol_length_request_or_respond(client_socket, "start: player number" + str(current_deck+1))
+                    protocol_length_request_or_respond(client_socket, "It's your turn, player number: " + str(current_deck+1))
                     while True:
                         data = protocol_decryption_request(client_socket)
                         break
@@ -39,7 +56,8 @@ def main():
                         numbers = data[0]
                         used_cards=data[1]
                         four_decks[current_deck] = data[2]
-                        client_socket.sendall(data)
+                        the_data = [numbers, used_cards, four_decks[current_deck]]
+                        protocol_length_request_or_respond(client_socket, the_data)
                 except socket.error as err:
                     print(f"Received socket error on client socket: {err}")
                     client_socket.close()
